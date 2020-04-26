d5b19a062a3501c970399b60cae582641a3c01b3
Krisztian KERN <krisztian.kern@shiwaforce.com>
2020-04-26 11:33:00 +0200

    mqtt cloud , will not use

---
+++ mqtt-gcloud/install_submodules.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+mkdir -p "${HOME}/.local"
+
+echo "Initializing submodules"
+git submodule update --init --recursive
+
+install_submodule () {
+  echo "Installing $1"
+
+  rm -rf "$1/build"
+  mkdir -p "$1/build"
+
+  cd "$1/build" || exit
+
+  cmake -DCMAKE_INSTALL_PREFIX:PATH="${HOME}/.local" -DPAHO_WITH_SSL=TRUE ..
+  make install
+
+  cd ../..
+
+  echo "#######################"
+  echo "Successfully installed $1 under ${HOME}/.local"
+  echo "#######################"
+}
+
+install_submodule "paho.mqtt.c"
--- mqtt-gcloud/main.c
+++ mqtt-gcloud/main.c
@@ -1,30 +1,94 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "MQTTClient.h"
+#include "openssl/evp.h"
+#include "iot_mqtt_jwt.h"
 
-#include <mqtt.h>
-#include "MQTT-C/examples/templates/openssl_sockets.h"
-
-#define MQTT_HOST "mqtt.googleapis.com"
-#define MQTT_PORT 8883
-#define MQTT_CLIENT_ID "projects/team2-275116/locations/eu-west1/registries/team2-device-registry/devices/team2-test-device"
-
-void publish_callback(void** unused, struct mqtt_response_publish *published);
+struct {
+    char* address;
+    char client_id[256];
+    char* device_id;
+    char* key_path;
+    char* project_id;
+    char* region;
+    char* registry_id;
+    char* root_path;
+    char topic[256];
+    char* payload;
+    char* algorithm;
+} opts = {.address = "ssl://mqtt.googleapis.com:8883",
+        .client_id =
+        "projects/team2-275116/locations/europe-west1/"
+        "registries/team2-device-registry/devices/team2-test-device",
+        .device_id = "team2-test-device",
+        .key_path = "/home/kkern/elte/ARI/2019-2020-2/RTOS/rsa_private.pem",
+        .project_id = "team2-275116",
+        .region = "europe-west1",
+        .registry_id = "team2-device-registry",
+        .root_path = "/home/kkern/elte/ARI/2019-2020-2/RTOS/roots.pem",
+        .topic = "projects/team2-275116/topics/test-topic",
+        .payload = "Hello world!",
+        .algorithm = "RS256"};
 
 int main() {
-    printf("Hello, World!\n");
+    char* payload = "Hello, World!";
 
-    uint8_t sendbuf[2048]; /* sendbuf should be large enough to hold multiple whole mqtt messages */
-    uint8_t recvbuf[1024]; /* recvbuf should be large enough any whole mqtt message expected to be received */
+    int rc = - 1;
+    MQTTClient client = {0};
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
 
-    struct mqtt_client client;
+    MQTTClient_create(&client, opts.address, opts.client_id,
+                      MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    conn_opts.keepAliveInterval = 60;
+    conn_opts.cleansession = 1;
+    conn_opts.username = "unused";
+    conn_opts.password = CreateJwt(opts.key_path, opts.project_id, opts.algorithm);
+    MQTTClient_SSLOptions sslopts = MQTTClient_SSLOptions_initializer;
 
-    // TODO linuxon
-    //mqtt_init(&client, sockfd, sendbuf, sizeof(sendbuf), recvbuf, sizeof(recvbuf), publish_callback);
+    sslopts.trustStore = opts.root_path;
+    sslopts.privateKey = opts.key_path;
+    conn_opts.ssl = &sslopts;
 
+    unsigned long retry_interval_ms = 500L;
+    unsigned long total_retry_time_ms = 0;
+    while ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
+        if (rc == 3) {  // connection refused: server unavailable
+            usleep(retry_interval_ms * 1000);
+            total_retry_time_ms += retry_interval_ms;
+            if (total_retry_time_ms >= 900000L) {
+                printf("Failed to connect, maximum retry time exceeded.");
+                exit(EXIT_FAILURE);
+            }
+            retry_interval_ms *= 1.5f;
+            if (retry_interval_ms > 6000L) {
+                retry_interval_ms = 6000L;
+            }
+        } else {
+            printf("Failed to connect, return code %d\n", rc);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    MQTTClient_deliveryToken token = {0};
+
+    pubmsg.payload = payload;
+    pubmsg.payloadlen = strlen(payload);
+    pubmsg.qos = 1;
+    pubmsg.retained = 0;
+    MQTTClient_publishMessage(client, opts.topic, &pubmsg, &token);
+    printf(
+            "Waiting for up to %lu seconds for publication of %s\n"
+            "on topic %s for client with ClientID: %s\n",
+            (10000L / 1000), opts.payload, opts.topic, opts.client_id);
+    rc = MQTTClient_waitForCompletion(client, token, 10000L);
+    printf("Message with delivery token %d delivered\n", token);
+
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
+
+    EVP_cleanup();
     return 0;
-}
-
-void publish_callback(void** unused, struct mqtt_response_publish *published)
-{
-    /* not used in this example */
 }
