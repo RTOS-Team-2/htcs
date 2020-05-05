479262976aca42f1bcc10a875ac3b694ee4a0032
Krisztian KERN <krisztian.kern@shiwaforce.com>
2020-05-05 22:18:08 +0200

    opening curlies

--- htcs-vehicle/src/mutex.c
+++ htcs-vehicle/src/mutex.c
@@ -1,7 +1,6 @@
 #include "mutex.h"
 
-int mutex_init(MUTEX *mutex)
-{
+int mutex_init(MUTEX *mutex) {
     #if defined(_WIN32)
     *mutex = CreateMutex(0, FALSE, 0);
     return (*mutex==0);
@@ -10,8 +9,7 @@ int mutex_init(MUTEX *mutex)
     #endif
 }
 
-int mutex_lock(MUTEX *mutex)
-{
+int mutex_lock(MUTEX *mutex) {
     #if defined(_WIN32)
     return (WaitForSingleObject(*mutex, INFINITE)==WAIT_FAILED?1:0);
     #else
