08d83077231feed93d874de5c51b35ded52997e0
Kotan Tamas <33610984+torta211@users.noreply.github.com>
2020-05-07 21:31:38 +0200

    start

--- python/HTCSPythonUtil.py
+++ python/HTCSPythonUtil.py
@@ -17,7 +17,6 @@ def get_connection_config():
                        in open(os.path.dirname(os.path.abspath(__file__)) + "/connection.properties")
                        if not l.strip().startswith("#") )
     config_dict["position_bound"] = int(config_dict["position_bound"])
-    config_dict["max_car_size"] = int(config_dict["max_car_size"])
     config_dict["quality_of_service"] = int(config_dict["quality_of_service"])
     config_dict["base_topic"] = config_dict["listening_topic"][:-2]
     return config_dict
--- python/visu.py
+++ python/visu.py
@@ -15,154 +15,81 @@ logger = logging.getLogger(__name__)
 CONNECTION_CONFIG = get_connection_config()
 local_cars = {}
 # image resources
-WINDOW_NAME = "Highway Traffic Control System Visualization"
-im_map = cv2.imread(os.path.dirname(os.path.abspath(__file__)) + "/res/map.png")
-red_car_straight = cv2.imread(os.path.dirname(os.path.abspath(__file__)) + "/res/car1.png")
-red_car_left = cv2.imread(os.path.dirname(os.path.abspath(__file__)) + "/res/car1left.png")
-red_car_right = cv2.imread(os.path.dirname(os.path.abspath(__file__)) + "/res/car1right.png")
-blue_car_straight = cv2.imread(os.path.dirname(os.path.abspath(__file__)) + "/res/car2.png")
-blue_car_left = cv2.imread(os.path.dirname(os.path.abspath(__file__)) + "/res/car2left.png")
-blue_car_right = cv2.imread(os.path.dirname(os.path.abspath(__file__)) + "/res/car2right.png")
-# for calculations
-map_length = im_map.shape[1]
-center_fast_lane = 180
-center_slow_lane = 300
-center_merge_lane = 420
-max_car_size_pixel = int(0.86 * (center_slow_lane - center_fast_lane))
-# for navigation
-current_offset = 0
-current_region_width = map_length
-commands = dict([(0, "Maintain speed!"), (1, "Accelerate!"), (2, "Brake!"), (3, "Switch lanes!"), (4, "Terminate!")])
+WINDOW_NAME_MINIMAP = "Highway Traffic Control System Minimap"
+WINDOW_NAME_VISU = "Highway Traffic Control System Visualization"
+im_bigmap = cv2.imread(os.path.dirname(os.path.abspath(__file__)) + "/res/map_big.png")
+im_minimap = cv2.imread(os.path.dirname(os.path.abspath(__file__)) + "/res/minimap.png")
+# view managing
+# will never change
+minimap_length_pixel = im_minimap.shape[1]
+minimap_height_pixel = im_minimap.shape[0]
+bigmap_length_pixel = im_bigmap.shape[1]
+map_length_meter = CONNECTION_CONFIG["position_bound"]
+map_width_meter = 19
+x_scale_minimap = minimap_length_pixel / map_length_meter
+x_scale_bigmap = bigmap_length_pixel / map_length_meter
+# will change
+go_on = True
+is_dragging = False
+offset_meter = 0
+region_width_meter = 250
+offset_minimap_pixel = int(offset_meter * x_scale_minimap)
+region_width_minimap_pixel = int(region_width_meter * x_scale_minimap)
+drag_start_x = 0
+drag_start_offset = 0
 
 
-def on_message_vis(mqttc, obj, msg):
-    topic_parts = msg.topic.split('/')
-    if topic_parts[1] == "vehicles":
-        car_id = topic_parts[-2]
-        msg_type = topic_parts[-1]
-        if msg_type == "join":
-            if car_id in local_cars.keys():
-                logger.warning(f"Car with already existing id ({car_id}) sent a join message")
-            else:
-                try:
-                    specs = ast.literal_eval("{" + msg.payload.decode("utf-8") + "}")
-                    local_cars[car_id] = CarImage(0, 0, specs['size'])
-                    logger.info(f"Car with {car_id} joined traffic")
-                except TypeError:
-                    logger.warning(f"Received a badly formatted join message from id {car_id}: {msg.payload.decode('utf-8')}")
-        elif msg_type == "state":
-            if car_id not in local_cars.keys():
-                logger.warning(f"Car with unrecognized id ({car_id}) sent a state message")
-            else:
-                try:
-                    state = ast.literal_eval("{" + msg.payload.decode("utf-8") + "}")
-                    local_cars[car_id].car.update_state(**state)
-                except TypeError:
-                    logger.warning(f"Received a badly formatted state message from id {car_id}: {msg.payload.decode('utf-8')}")
-        elif msg_type == "command":
-            if car_id in local_cars.keys():
-                logger.info(f"Car with id {car_id} received a command: {commands[int(msg.payload.decode('utf-8'))]}")
-            else:
-                logger.warning(f"Car with unrecognized id ({car_id}) received a command: {commands[int(msg.payload.decode('utf-8'))]}")
-        else:
-            logger.warning(f"Unrecognized topic: {msg_type}")
-
-
-# class for visualization
-class CarImage:
-    def __init__(self, distance_taken, lane, size):
-        # Create Car
-        self.car = Car.for_visualization(distance_taken, lane, size)
-        # Red or Blue
-        if bool(random.getrandbits(1)):
-            self.straight = red_car_straight
-            self.left = red_car_left
-            self.right = red_car_right
-        else:
-            self.straight = blue_car_straight
-            self.left = blue_car_left
-            self.right = blue_car_right
-        # Scale to correct size
-        orig_h, orig_w = self.straight.shape[:2]
-        new_h = np.round(max_car_size_pixel * self.car.specs.size / CONNECTION_CONFIG["max_car_size"]).astype(np.int32)
-        new_w = np.round(orig_w * new_h / orig_h).astype(np.int32)
-        self.straight = cv2.resize(self.straight, (new_w, new_h))
-        self.left = cv2.resize(self.left, (new_w, new_h))
-        self.right = cv2.resize(self.right, (new_w, new_h))
-
-    def get_y_slice(self):
-        if self.car.lane == 0:
-            start = int(center_merge_lane - self.straight.shape[0] / 2)
-        elif self.car.lane == 1:
-            start = int((center_merge_lane + center_slow_lane) / 2 - self.straight.shape[0] / 2)
-        elif self.car.lane == 2:
-            start = int(center_slow_lane - self.straight.shape[0] / 2)
-        elif self.car.lane in [3, 4]:
-            start = int((center_slow_lane + center_fast_lane) / 2 - self.straight.shape[0] / 2)
-        elif self.car.lane == 5:
-            start = int(center_fast_lane - self.straight.shape[0] / 2)
-        return slice(start, start + self.straight.shape[0])
-
-    def get_x_slice(self):
-        possible_maximum = map_length - self.straight.shape[1]
-        start = np.floor(possible_maximum * self.car.get_relative_position(CONNECTION_CONFIG)).astype(np.int32)
-        return slice(start, start + self.straight.shape[1])
-
-    def get_image(self):
-        if self.car.lane in [0, 2, 5]:
-            return self.straight
-        elif self.car.lane in [1, 3]:
-            return self.left
-        elif self.car.lane == 4:
-            return self.right
-
-
-def display_state(cars: List[CarImage]):
-    global current_offset, current_region_width
-
-    vis = im_map.copy()
-    for car in cars:
-        vis[car.get_y_slice(), car.get_x_slice(), :] = car.get_image()
-
-    c = cv2.waitKey(16)
-    if c == ord('a'):
-        current_offset -= 30
-    elif c == ord('d'):
-        current_offset += 30
-    elif c == ord('w'):
-        if current_region_width > map_length * 0.1:
-            current_region_width = np.floor(current_region_width * 0.95).astype(np.int32)
-            _, _, wW, wH = cv2.getWindowImageRect(WINDOW_NAME)
-            cv2.resizeWindow(WINDOW_NAME, wW, int(wH / 0.95))
-    elif c == ord('s'):
-        new_region_width = np.floor(current_region_width / 0.95).astype(np.int32)
-        if new_region_width <= map_length:
-            current_region_width = new_region_width
-            _, _, window_w, window_h = cv2.getWindowImageRect(WINDOW_NAME)
-            cv2.resizeWindow(WINDOW_NAME, window_w, int(window_h * 0.95))
-        else:
-            current_region_width = map_length
-    elif c == ord('x'):
-        return False
-    current_offset = max(min(current_offset, map_length - current_region_width), 0)
-
-    cv2.imshow(WINDOW_NAME, vis[:, current_offset: current_offset + current_region_width, :])
-    return True
+def minimap_move(event, x, y, flags, param):
+    global offset_minimap_pixel, drag_start_x, drag_start_offset, is_dragging, offset_meter
+    if event == cv2.EVENT_LBUTTONDOWN and offset_minimap_pixel <= x <= offset_minimap_pixel + region_width_minimap_pixel:
+        drag_start_x = x
+        drag_start_offset = offset_minimap_pixel
+        is_dragging = True
+    elif event == cv2.EVENT_MOUSEMOVE and is_dragging:
+        offset_minimap_pixel = max(0, min(drag_start_offset + x - drag_start_x, minimap_length_pixel))
+        offset_meter = offset_minimap_pixel / x_scale_minimap
+    elif event == cv2.EVENT_LBUTTONUP:
+        is_dragging = False
 
 
 if __name__ == "__main__":
-    setup_connector(CONNECTION_CONFIG, on_message=on_message_vis)
-    mqtt_connector.loop_start()
+    cv2.namedWindow(WINDOW_NAME_MINIMAP, cv2.WINDOW_FREERATIO)
+    cv2.resizeWindow(WINDOW_NAME_MINIMAP, 1700, 150)
+    cv2.setMouseCallback(WINDOW_NAME_MINIMAP, minimap_move)
 
-    cv2.namedWindow(WINDOW_NAME, flags=cv2.WINDOW_NORMAL)
-    cv2.resizeWindow(WINDOW_NAME, 1800, 250)
+    cv2.namedWindow(WINDOW_NAME_VISU, cv2.WINDOW_FREERATIO)
+    region_aspect_ratio = region_width_meter / map_width_meter
+    cv2.resizeWindow(WINDOW_NAME_VISU, 1800, int(1800 / region_aspect_ratio))
 
-    lock = threading.Lock()
-
-    go_on = True
     while go_on:
-        with lock:
-            go_on = display_state(list(local_cars.values()))
-
-    mqtt_connector.loop_stop()
+        cur_im_minimap = im_minimap.copy()
+        # gray out
+        cur_im_minimap[:, : offset_minimap_pixel, :] = \
+            (cur_im_minimap[:, : offset_minimap_pixel, :] * 0.6).astype(np.int32)
+        cur_im_minimap[:, offset_minimap_pixel + region_width_minimap_pixel:, :] = \
+            (cur_im_minimap[:, offset_minimap_pixel + region_width_minimap_pixel:, :] * 0.6).astype(np.int32)
+        # scale up
+        cur_im_minimap[:, offset_minimap_pixel: offset_minimap_pixel + region_width_minimap_pixel, :] = \
+            cv2.resize(cur_im_minimap[int(minimap_height_pixel * 0.1): int(minimap_height_pixel * 0.75),
+                                      offset_minimap_pixel: offset_minimap_pixel + region_width_minimap_pixel,
+                                      :],
+                       (region_width_minimap_pixel, minimap_height_pixel))
+        # orange lines
+        cv2.line(cur_im_minimap,
+                 (offset_minimap_pixel, 0),
+                 (offset_minimap_pixel, minimap_height_pixel),
+                 (0, 140, 255),
+                 3)
+        cv2.line(cur_im_minimap,
+                 (offset_minimap_pixel + region_width_minimap_pixel, 0),
+                 (offset_minimap_pixel + region_width_minimap_pixel, minimap_height_pixel),
+                 (0, 140, 255),
+                 3)
+        cv2.imshow(WINDOW_NAME_MINIMAP, cur_im_minimap)
+        offset_bigmap_pixel = int(offset_meter * x_scale_bigmap)
+        region_width_bigmap_pixel = int(region_width_meter * x_scale_bigmap)
+        cv2.imshow(WINDOW_NAME_VISU, im_bigmap[:,
+                                               offset_bigmap_pixel: offset_bigmap_pixel + region_width_bigmap_pixel,
+                                               :])
+        cv2.waitKey(16)
 
