0e6d29125f322c8978562eb9aff6cf1c6032575d
Krisztian KERN <krisztian.kern@shiwaforce.com>
2020-05-07 18:01:22 +0200

    linux compatible python vehicle generator

--- python/generator.py
+++ python/generator.py
@@ -3,13 +3,16 @@ import uuid
 import time
 import signal
 import random
+import pathlib
+import datetime
 import subprocess
 from HTCSPythonUtil import get_connection_config
 
 
 repo_root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 executable_name_windows = repo_root_dir + "/htcs-vehicle/Debug/htcs-vehicle.exe"
-executable_name_linux = "TODO"
+executable_name_linux = repo_root_dir + "/htcs-vehicle/build/htcs_vehicle"
+logs_dir = repo_root_dir + "/python/logs"
 CONNECTION_CONFIG = get_connection_config()
 GENERATE_TIME_INTERVAL_MIN = 5
 GENERATE_TIME_INTERVAL_WIDTH = 7
@@ -24,6 +27,8 @@ BRAKING_POWER_INTERVAL_WIDTH = 11
 SIZE_INTERVAL_MIN = 2
 SIZE_INTERVAL_WIDTH = CONNECTION_CONFIG["max_car_size"] - SIZE_INTERVAL_MIN       # uh...  too many connections :D
 running_children = []
+
+os.putenv("LD_LIBRARY_PATH", os.getenv("HOME") + "/Eclipse-Paho-MQTT-C-1.3.1-Linux/lib")
 
 
 class GracefulKiller:
@@ -41,15 +46,18 @@ class GracefulKiller:
 
 
 if __name__ == "__main__":
-    print(executable_name_windows)
     killer = GracefulKiller()
-    file_h = open("cars_log.txt", "w")
-    while not killer.kill_now and len(running_children) < 2:
+    now = datetime.datetime.now()
+    now_str = now.strftime('%Y%m%d%H%M%S')
+    current_logs_dir = logs_dir + "/generation-" + now_str
+    pathlib.Path(current_logs_dir).mkdir(exist_ok=True, parents=True)
+    while not killer.kill_now:
         sleep_time = random.random() * GENERATE_TIME_INTERVAL_WIDTH + GENERATE_TIME_INTERVAL_MIN
+        client_id = str(uuid.uuid4())
         params_string = f"--address {CONNECTION_CONFIG['address']} " \
                         f"--username {CONNECTION_CONFIG['username']} " \
                         f"--password {CONNECTION_CONFIG['password']} " \
-                        f"--clientId {str(uuid.uuid4())} " \
+                        f"--clientId {client_id} " \
                         f"--topic {CONNECTION_CONFIG['base_topic']} " \
                         f"--preferredSpeed {random.random() * PREF_SPEED_INTERVAL_WIDTH + PREF_SPEED_INTERVAL_MIN} " \
                         f"--maxSpeed {random.random() * MAX_SPEED_INTERVAL_WIDTH + MAX_SPEED_INTERVAL_MIN} " \
@@ -57,12 +65,11 @@ if __name__ == "__main__":
                         f"--brakingPower {random.random() * BRAKING_POWER_INTERVAL_WIDTH + BRAKING_POWER_INTERVAL_MIN} " \
                         f"--size {random.random() * SIZE_INTERVAL_WIDTH + SIZE_INTERVAL_MIN}"
 
+        file_h = open(current_logs_dir + "/htcs_vehicle-" + client_id + ".log", "w")
         if os.name == 'nt':
             running_children.append(subprocess.Popen(executable_name_windows + " " + params_string, stdout=file_h))
         else:
-            raise NotImplementedError("implement linux call")
+            running_children.append(subprocess.Popen(executable=executable_name_linux, args=params_string.split(' '),
+                                                     shell=True, stdout=file_h, stderr=file_h))
 
         time.sleep(sleep_time)
-
-    while not killer.kill_now:
-        time.sleep(1)
