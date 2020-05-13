d28a7cce2bafd44834295cd93dd6cb833a98443a
Kotan Tamas <33610984+torta211@users.noreply.github.com>
2020-05-13 13:35:25 +0200

    save

--- python/visu.py
+++ python/visu.py
@@ -18,9 +18,15 @@ region_width_bigmap_pixel = int(region_width_meter * vis.x_scale_bigmap)
 current_detail_height = vis.detail_height
 # navigation variables
 focused_car: vis.CarImage
+x_slice_focused: slice
+image_focused: np.ndarray
 is_dragging = False
 drag_start_x = 0
 drag_start_offset = 0
+# text management
+text_region_height = 30
+text_color = (0, 0, 255)
+# some info
 logger.info(f"Full length of the map is {vis.map_length_meter} m.")
 logger.info(f"Current visible region is {vis.region_width_meter_start} m wide.")
 logger.info(f"Press <w> key to zoom in, press <s> key to zoom out.")
@@ -82,6 +88,83 @@ def on_terminate(client, userdata, message):
         _car.exploded = True
 
 
+def follow_with_camera():
+    global focused_car, x_slice_focused, image_focused, offset_meter, offset_minimap_pixel, offset_bigmap_pixel
+    if focused_car is not None:
+        if focused_car.distance_taken + region_width_meter / 2 > vis.map_length_meter - 5 \
+                or focused_car.exploded:
+            focused_car = None
+        else:
+            offset_meter = max(0, focused_car.distance_taken - region_width_meter / 2)
+            x_slice_focused, image_focused = focused_car.get_x_slice_and_image(offset_meter, region_width_meter)
+            offset_minimap_pixel = int(offset_meter * vis.x_scale_minimap)
+            offset_bigmap_pixel = int(offset_meter * vis.x_scale_bigmap)
+
+
+def put_on_title():
+    # put title in cone
+    canvas[vis.minimap_height_pixel: vis.minimap_height_pixel + vis.black_region_height, :, :] = vis.title
+    cv2.fillConvexPoly(canvas, np.int32(((0, vis.minimap_height_pixel),
+                                         (0, vis.minimap_height_pixel + vis.black_region_height),
+                                         (offset_minimap_pixel, vis.minimap_height_pixel))), (0, 0, 0))
+    cv2.fillConvexPoly(canvas, np.int32(((offset_minimap_pixel + region_width_minimap_pixel, vis.minimap_height_pixel),
+                                         (vis.window_width, vis.minimap_height_pixel + vis.black_region_height),
+                                         (vis.window_width, vis.minimap_height_pixel))), (0, 0, 0))
+
+
+def set_minimap():
+    # gray out
+    canvas[:vis.minimap_height_pixel, : offset_minimap_pixel, :] = \
+        (vis.im_minimap[:, : offset_minimap_pixel, :] * 0.6).astype(np.int32)
+    canvas[:vis.minimap_height_pixel, offset_minimap_pixel + region_width_minimap_pixel:, :] = \
+        (vis.im_minimap[:, offset_minimap_pixel + region_width_minimap_pixel:, :] * 0.6).astype(np.int32)
+    # zoom in
+    canvas[:vis.minimap_height_pixel, offset_minimap_pixel: offset_minimap_pixel + region_width_minimap_pixel, :] = \
+        cv2.resize(vis.im_minimap[int(vis.minimap_height_pixel * 0.05): int(vis.minimap_height_pixel * 0.94),
+                   offset_minimap_pixel: offset_minimap_pixel + region_width_minimap_pixel, :],
+                   (region_width_minimap_pixel, vis.minimap_height_pixel))
+
+
+def draw_orange_lines():
+    cv2.line(canvas,
+             (offset_minimap_pixel, 0),
+             (offset_minimap_pixel, vis.minimap_height_pixel),
+             (0, 211, 255),
+             3)
+    cv2.line(canvas,
+             (offset_minimap_pixel + region_width_minimap_pixel, 0),
+             (offset_minimap_pixel + region_width_minimap_pixel, vis.minimap_height_pixel),
+             (0, 211, 255),
+             3)
+    cv2.line(canvas,
+             (offset_minimap_pixel, vis.minimap_height_pixel),
+             (0, vis.minimap_height_pixel + vis.black_region_height),
+             (0, 211, 255),
+             3)
+    cv2.line(canvas,
+             (offset_minimap_pixel + region_width_minimap_pixel, vis.minimap_height_pixel),
+             (vis.window_width, vis.minimap_height_pixel + vis.black_region_height),
+             (0, 211, 255),
+             3)
+
+
+def put_on_focused_car_stats():
+    y = vis.minimap_height_pixel + vis.black_region_height + 50
+    cv2.putText(canvas, f"id={focused_car.id}", (5, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+    y += 33
+    cv2.putText(canvas, f"speed={focused_car.speed} [m/s]", (5, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+    y += 33
+    cv2.putText(canvas, f"prefSpeed={focused_car.specs.preferred_speed}", (5, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+    y += 33
+    cv2.putText(canvas, f"maxSpeed={focused_car.specs.max_speed}", (5, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+    y += 33
+    cv2.putText(canvas, f"accState={AccelerationState(focused_car.acceleration_state).name}", (5, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+    y += 33
+    cv2.putText(canvas, f"brakePower={focused_car.specs.braking_power} [m/s^2]", (5, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+    y += 33
+    cv2.putText(canvas, f"followDist={focused_car.follow_distance()}", (5, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+
+
 if __name__ == "__main__":
     local_cars = DetailedCarTracker()
     focused_car = None
@@ -96,34 +179,10 @@ if __name__ == "__main__":
         canvas = np.zeros((vis.minimap_height_pixel + vis.black_region_height + current_detail_height + 30,
                            vis.window_width, 3),
                           np.uint8)
-        # follow with camera
-        if focused_car is not None:
-            if focused_car.distance_taken + region_width_meter / 2 > vis.map_length_meter - 5 \
-                    or focused_car.exploded:
-                focused_car = None
-            else:
-                offset_meter = max(0, focused_car.distance_taken - region_width_meter / 2)
-                x_slice_focused, image_focused = focused_car.get_x_slice_and_image(offset_meter, region_width_meter)
-                offset_minimap_pixel = int(offset_meter * vis.x_scale_minimap)
-                offset_bigmap_pixel = int(offset_meter * vis.x_scale_bigmap)
-        # put title in cone
-        canvas[vis.minimap_height_pixel: vis.minimap_height_pixel + vis.black_region_height, :, :] = vis.title
-        cv2.fillConvexPoly(canvas, np.int32(((0, vis.minimap_height_pixel),
-                                             (0, vis.minimap_height_pixel + vis.black_region_height),
-                                             (offset_minimap_pixel, vis.minimap_height_pixel))), (0, 0, 0))
-        cv2.fillConvexPoly(canvas, np.int32(((offset_minimap_pixel + region_width_minimap_pixel, vis.minimap_height_pixel),
-                                             (vis.window_width, vis.minimap_height_pixel + vis.black_region_height),
-                                             (vis.window_width, vis.minimap_height_pixel))), (0, 0, 0))
-        # gray out
-        canvas[:vis.minimap_height_pixel, : offset_minimap_pixel, :] = \
-            (vis.im_minimap[:, : offset_minimap_pixel, :] * 0.6).astype(np.int32)
-        canvas[:vis.minimap_height_pixel, offset_minimap_pixel + region_width_minimap_pixel:, :] = \
-            (vis.im_minimap[:, offset_minimap_pixel + region_width_minimap_pixel:, :] * 0.6).astype(np.int32)
-        # zoom in
-        canvas[:vis.minimap_height_pixel, offset_minimap_pixel: offset_minimap_pixel + region_width_minimap_pixel, :] = \
-            cv2.resize(vis.im_minimap[int(vis.minimap_height_pixel * 0.05): int(vis.minimap_height_pixel * 0.94),
-                                      offset_minimap_pixel: offset_minimap_pixel + region_width_minimap_pixel, :],
-                       (region_width_minimap_pixel, vis.minimap_height_pixel))
+        follow_with_camera()
+        put_on_title()
+        set_minimap()
+        draw_orange_lines()
         # get current part of the map
         cur_im_detail = cv2.resize(vis.im_bigmap[:, offset_bigmap_pixel:offset_bigmap_pixel + region_width_bigmap_pixel, :],
                                    (vis.window_width, vis.detail_height),
@@ -138,47 +197,15 @@ if __name__ == "__main__":
         if focused_car is not None:
             cur_im_detail[focused_car.get_y_slice(), x_slice_focused, :] = image_focused
 
-        # orange lines
-        cv2.line(canvas,
-                 (offset_minimap_pixel, 0),
-                 (offset_minimap_pixel, vis.minimap_height_pixel),
-                 (0, 211, 255),
-                 3)
-        cv2.line(canvas,
-                 (offset_minimap_pixel + region_width_minimap_pixel, 0),
-                 (offset_minimap_pixel + region_width_minimap_pixel, vis.minimap_height_pixel),
-                 (0, 211, 255),
-                 3)
-        cv2.line(canvas,
-                 (offset_minimap_pixel, vis.minimap_height_pixel),
-                 (0, vis.minimap_height_pixel + vis.black_region_height),
-                 (0, 211, 255),
-                 3)
-        cv2.line(canvas,
-                 (offset_minimap_pixel + region_width_minimap_pixel, vis.minimap_height_pixel),
-                 (vis.window_width, vis.minimap_height_pixel + vis.black_region_height),
-                 (0, 211, 255),
-                 3)
         # set correct height
         canvas[vis.minimap_height_pixel + vis.black_region_height: -30, :, :] = \
             cv2.resize(cur_im_detail, (vis.window_width, current_detail_height))
 
-        # put frame time
         if focused_car is not None:
-            y = vis.minimap_height_pixel + vis.black_region_height + 50
-            cv2.putText(canvas, f"id={focused_car.id}", (5, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-            y += 33
-            cv2.putText(canvas, f"speed={focused_car.speed} [m/s]", (5, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-            y += 33
-            cv2.putText(canvas, f"prefSpeed={focused_car.specs.preferred_speed}", (5, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-            y += 33
-            cv2.putText(canvas, f"accState={AccelerationState(focused_car.acceleration_state).name}", (5, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-            y += 33
-            cv2.putText(canvas, f"brakePower={focused_car.specs.braking_power} [m/s^2]", (5, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-            y += 33
-            cv2.putText(canvas, f"followDist={focused_car.follow_distance()}", (5, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-        fps_text = f"FPS: {np.floor(1 / (time.time() - frame_start))}"
-        cv2.putText(canvas, fps_text, (5, canvas.shape[0] - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            put_on_focused_car_stats()
+        # put frame time
+        cv2.putText(canvas, f"FPS: {np.floor(1 / (time.time() - frame_start + 0.0001))}",
+                    (5, canvas.shape[0] - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
         cv2.imshow(vis.WINDOW_NAME, canvas)
         key = cv2.waitKey(1)
